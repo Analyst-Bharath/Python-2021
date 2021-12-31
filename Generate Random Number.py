2c5059e47e6140dd3144ed9375d585c8194b32c0
Bharath G <bharath021297@gmail.com>
2021-12-31 12:28:46 +0530

    Change Commit

--- Generate Random Number.py
+++ Generate Random Number.py
@@ -1,3 +1,16 @@
 print("To Generate Random Prime Number ")
 
-import random
+#for prime_number in an interval set the loer and upper limit
+
+lower = int(input("Enter lower limit: "))
+upper = int(input("Enter upper limit: "))
+
+
+for num in range(lower,upper+1):
+    if num > 1:
+        for i in range (2,num):
+            if (num%i)==0:
+                break
+
+        else:
+            print(num)
